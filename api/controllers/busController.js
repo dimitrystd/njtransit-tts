f6f7a69cc9043b75ffc421614c27870630da598d
Dmitriy Studinskiy <dms@smartling.com>
2017-12-26 17:33:51 -0500

    Added tests

--- api/controllers/busController.js
+++ api/controllers/busController.js
@@ -1,24 +1,38 @@
-var request = require('sync-request');
-var xml2js = require('xml2js');
+const request = require('sync-request');
+const parseStringSync = require('xml2js-parser').parseStringSync;
 
-exports.get_eta = function(req, res) {
+function get_eta (req, res) {
     var route = req.params.route;
     var stop = req.params.stop;
     var response = request('GET', `http://mybusnow.njtransit.com/bustime/eta/getStopPredictionsETA.jsp?route=${route}&stop=${stop}`);
     if (response && response.statusCode > 200) {
         res.send(`Could not receive bus ETA. I got status code: ${response.statusCode}`);
     } else {
-        var parser = new xml2js.Parser();
-        parser.parseString(response.body, function (err, result) {
-            if (err) {
-                res.send(`Could not parse data with bus schedule. I got error: ${err}`);
-            } else {
-                if (result['stop']['noPredictionMessage']) {
-                    res.send(`There is not schedule for bus ${route}`);
-                } else {
-                    res.send('not implemented');
-                }
-            }
-        });
+        res.send(parseBusSchedule(response.body));
     }
 };
+
+function parseBusSchedule (xml) {
+    var result = parseStringSync(xml, {normalize: true});
+    if (result['stop']['noPredictionMessage']) {
+        return `There is not schedule for bus`;
+    } else {
+        if (!result['stop'] || !result['stop']['pre'] || result['stop']['pre'].length == 0) {
+            return `Could not parse data with bus schedule. It doesn't contain information about routs`;
+        } else {
+            var firstBus = result['stop']['pre'][0];
+            var interval = firstBus.pt[0] + firstBus.pu[0].toLowerCase();
+            var time = firstBus.nextbusonroutetime;
+            if (firstBus.pt < 6) {
+                return `Bus will arrive in ${interval}, at ${time}. Hurry up`;
+            } else {
+                return `Bus will arrive in ${interval}, at ${time}`;
+            }
+        }
+    }
+}
+
+module.exports = {
+    get_eta,
+    parseBusSchedule
+}
---
+++ test/busController.js
@@ -0,0 +1,22 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+var busController = require('../api/controllers/busController');
+
+describe('busController', function() {
+    describe('Parse XML', function() {
+        it('No schedule', function() {
+            var xmlFile = fs.readFileSync('test/resources/noServiceScheduled.xml');
+            expect(busController.parseBusSchedule(xmlFile)).to.contains('There is not schedule for bus');
+        });
+
+        it('Unexpected xml structure', function() {
+            var xmlFile = fs.readFileSync('test/resources/invalidXmlStructure.xml');
+            expect(busController.parseBusSchedule(xmlFile)).to.contains('Could not parse data');
+        });
+
+        it('Will arrive less than 6 mins', function() {
+            var xmlFile = fs.readFileSync('test/resources/busWillArriveIn5min.xml');
+            expect(busController.parseBusSchedule(xmlFile)).to.contains('Hurry up');
+        });
+    });
+});
